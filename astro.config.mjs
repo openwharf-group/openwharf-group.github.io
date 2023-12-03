7bea6089982cd08e3ab5ceb8310dff9cb850d412
丹坤 <yuankun.yk@alibaba-inc.com>
2023-12-03 17:54:37 +0800

    feat: 🎸 add virtual.d.ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,6 +21,7 @@ export default defineConfig({
 			title: 'My Docs',
 			components: {
 				TableOfContents: './src/components/starlight/TableOfContents.astro',
+				Header: './src/components/starlight/Header.astro',
 			},
 			editLink: {
 				baseUrl: 'https://github.com/withastro/starlight/edit/main/docs/',
@@ -38,23 +39,6 @@ export default defineConfig({
 		tailwind({ applyBaseStyles: false }),
 	],
 	markdown: {
-		// shikiConfig: {
-		// 	// 选择 Shiki 内置的主题（或添加你自己的主题）
-		// 	// https://github.com/shikijs/shiki/blob/main/docs/themes.md
-		// 	theme: 'dracula',
-		// 	// 另外，也提供了多种主题
-		// 	// https://github.com/antfu/shikiji#lightdark-dual-themes
-		// 	experimentalThemes: {
-		// 		light: 'github-light',
-		// 		dark: 'github-dark',
-		// 	},
-		// 	// 添加自定义语言
-		// 	// 注意：Shiki 内置了无数语言，包括 .astro！
-		// 	// https://github.com/shikijs/shiki/blob/main/docs/languages.md
-		// 	langs: [],
-		// 	// 启用自动换行，以防止水平滚动
-		// 	wrap: true,
-		// },
 		rehypePlugins: [
 			// 在这里添加 rehype-external-links 插件配置
 			[rehypeExternalLinks, {
