512ce95b4aca9fdecc30653af5c89e926fe00d7a
丹坤 <yuankun.yk@alibaba-inc.com>
2023-12-07 10:22:59 +0800

    feat: 🎸 update build.cjs

--- script/build.cjs
+++ script/build.cjs
@@ -2,7 +2,10 @@ const fs = require('fs');
 const starlightFullPath = require.resolve("@astrojs/starlight");
 const starlightPath = starlightFullPath.replace("/index.ts", "")
 
-
+/**
+ * @description: 替换 utils/route-data.ts
+ * 传递 categories 参数
+ */
 const replaceRouteData = () => {
 	const originFile = starlightPath + "/utils/route-data.ts";
 	const originContent = fs.readFileSync(originFile, 'utf-8');
@@ -12,10 +15,12 @@ const replaceRouteData = () => {
 	);
 	fs.writeFileSync(originFile, replacedContent, 'utf-8');
 }
-
 replaceRouteData();
 
-// 替换 navigation.ts
+/**
+ * @description: 替换 utils/navigation.ts
+ * 核心的 sidebar 函数构建
+ */
 const replaceNavigation = () => {
 	const originFile = starlightPath + "/utils/navigation.ts";
 	const originContent = fs.readFileSync(originFile, 'utf-8');
@@ -26,7 +31,6 @@ const replaceNavigation = () => {
 		const routes = getLocaleRoutes(locale);
 		const versionRegex = /\\/docs\\/(next|v[0-9]\\.[0-9]\\.[0-9]|v[0-9]\\.[0-9]|v[0-9]|[0-9]\\.[0-9]\\.[0-9]|[0-9]\\.[0-9]|[0-9])/;
 		const match = versionRegex.exec(pathname);
-
 		const version = match ? match[1] : 'latest';
 		if(categories && categories[version]){
 			return categories[version].map((group) => configItemToEntry(group, pathname, locale, routes));
@@ -37,38 +41,15 @@ const replaceNavigation = () => {
 replaceNavigation();
 
 
+
+/**
+ * @description: 替换 index.astro
+ * 1. 动态替换核心路由能力
+ * 2. 动态集成siderBar能力
+ */
 const replaceIndexAstro = () => {
 	const originFile = starlightPath + "/index.astro";
 	const replacedContent = fs.readFileSync('./script/template/index.txt', 'utf-8')
 	fs.writeFileSync(originFile, replacedContent.toString(), 'utf-8');
 }
 replaceIndexAstro();
-
-// export function getSidebar(pathname: string, locale: string | undefined): SidebarEntry[] {
-// 	const routes = getLocaleRoutes(locale);
-// 	if (config.sidebar) {
-// 		const regex = /\/docs\/(latest|next|(v[0-9]\.[0-9]\.[0-9])|(v[0-9]\.[0-9])|(v[0-9])|([0-9]\.[0-9]\.[0-9])|([0-9]\.[0-9])|([0-9]))/;
-// 		const match = regex.exec(pathname);
-// 		if (match && match[1]) {
-// 			return config.sidebar[match[1]].map((group) => configItemToEntry(group, pathname, locale, routes));
-// 		} else {
-// 			return config.sidebar.latest.map((group) => configItemToEntry(group, pathname, locale, routes));
-// 		}
-// 	} else {
-// 		const regex = /latest|next|(v[0-9]\.[0-9]\.[0-9])|(v[0-9]\.[0-9])|(v[0-9])|([0-9]\.[0-9]\.[0-9])|([0-9]\.[0-9])|([0-9])/;
-// 		const match = regex.exec(pathname);
-// 		let dirRoutes = routes;
-// 		if (match && match[0]) {
-// 			// console.log("---", match)
-// 			dirRoutes = routes.filter(item => {
-// 				return item.slug.split('/')[0] === match[0]
-// 			})
-// 			// console.log('dirRoutes', dirRoutes)
-// 		} else {
-// 			// console.log("===", match)
-// 		}
-
-// 		const tree = treeify(dirRoutes, locale || '');
-// 		return sidebarFromDir(tree, pathname, locale, false);
-// 	}
-// }
