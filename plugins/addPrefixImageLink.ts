f149daaab61c548b8a84d48636ad8653213f5f8f
梓莼 <huangzichun.hzc@alibaba-inc.com>
2023-12-07 16:38:02 +0800

    feat: 🎸 blog

--- plugins/addPrefixImageLink.ts
+++ plugins/addPrefixImageLink.ts
@@ -6,9 +6,10 @@ import { visit } from 'unist-util-visit';
 export default function remarkRemoveMdLinks() {
   return (tree) => {
       visit(tree, 'image', (node) => {
+        //   将博客的img链接替换。从相对路径替换至从public文件夹取
           if (node.url.startsWith('img')) {
-            node.url = './' + node.url
-          }
+            node.url = '/' + node.url
+          } 
     });
   };
 }
