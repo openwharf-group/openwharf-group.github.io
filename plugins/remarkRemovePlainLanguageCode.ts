2a272463275cd806510602dac2a87eacc8eb0cc6
sumiaomiao <smm01400976@alibaba-inc.com>
2023-12-07 17:39:07 +0800

    fix: remarkRemoveMdLinks博客中涉及语言兜底补充

--- plugins/remarkRemovePlainLanguageCode.ts
+++ plugins/remarkRemovePlainLanguageCode.ts
@@ -7,15 +7,39 @@ import { visit } from 'unist-util-visit';
 
 export default function remarkRemoveMdLinks() {
   return (tree) => {
-    visit(tree, 'code', (node) => {
-      if (node.lang === 'plain') {
-        node.lang = '';
+    visit(tree, "code", (node) => {
+      if (node.lang === "plain") {
+        node.lang = "";
       }
-      if (node.lang === 'basic') {
-          node.lang = '';
+      if (node.lang === "basic") {
+        node.lang = "";
       }
-      if (node.lang === 'SH') {
-        node.lang = 'sh';
+      if (node.lang === "SH" || node.lang === "Shell") {
+        node.lang = "sh";
+      }
+      if (node.lang === "$xslt") {
+        node.lang = "xsl";
+      }
+      if (node.lang === "JavaScript") {
+        node.lang = "javascript";
+      }
+      if (node.lang === "Bash") {
+        node.lang = "bash";
+      }
+      if (node.lang === "JSON") {
+        node.lang = "json";
+      }
+      if (node.lang === "Apache") {
+        node.lang = "apache";
+      }
+      if (node.lang === "PYTHON") {
+        node.lang = "python";
+      }
+      if (node.lang === "JAVA" || node.lang === "protobuf") {
+        node.lang = "java";
+      }
+      if (node.lang === "PHP") {
+        node.lang = "php";
       }
     });
   };
