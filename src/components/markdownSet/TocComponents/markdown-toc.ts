10a5d5321b617af4853bc6105525442b94a4d4fc
梓莼 <huangzichun.hzc@alibaba-inc.com>
2024-01-09 10:11:43 +0800

    fix: 🐛 fix md

--- src/components/markdownSet/TocComponents/markdown-toc.ts
+++ src/components/markdownSet/TocComponents/markdown-toc.ts
@@ -2,7 +2,7 @@ const PAGE_TITLE_ID = "_top"
 
 export class StarlightTOC extends HTMLElement {
 	private _current = this.querySelector('a[aria-current="true"]') as HTMLAnchorElement | null;
-	private minH = parseInt(this.dataset.minH || '1', 10);
+	private minH = parseInt(this.dataset.minH || '2', 10);
 	private maxH = parseInt(this.dataset.maxH || '3', 10);
 
 	protected set current(link: HTMLAnchorElement) {
@@ -16,14 +16,13 @@ export class StarlightTOC extends HTMLElement {
 		super();
 
 		/** All the links in the table of contents. */
-        const links = [...this.querySelectorAll('a')];
-        
+		const links = [...this.querySelectorAll('a')];
 
 		/** Test if an element is a table-of-contents heading. */
 		const isHeading = (el: Element): el is HTMLHeadingElement => {
 			if (el instanceof HTMLHeadingElement) {
 				// Special case for page title h1
-                if (el.id === PAGE_TITLE_ID) return true;
+				if (el.id === PAGE_TITLE_ID) return true;
 				// Check the heading level is within the user-configured limits for the ToC
 				const level = el.tagName[1];
 				if (level) {
@@ -35,11 +34,10 @@ export class StarlightTOC extends HTMLElement {
 		};
 
 		/** Walk up the DOM to find the nearest heading. */
-        const getElementHeading = (el: Element | null): HTMLHeadingElement | null => {
-            // debugger;
+		const getElementHeading = (el: Element | null): HTMLHeadingElement | null => {
 			if (!el) return null;
 			const origin = el;
-            while (el) {
+			while (el) {
 				if (isHeading(el)) return el;
 				// Assign the previous sibling’s last, most deeply nested child to el.
 				el = el.previousElementSibling;
@@ -55,11 +53,10 @@ export class StarlightTOC extends HTMLElement {
 		};
 
 		/** Handle intersections and set the current link to the heading for the current intersection. */
-        const setCurrent: IntersectionObserverCallback = (entries) => {
-            for (const { isIntersecting, target } of entries) {
+		const setCurrent: IntersectionObserverCallback = (entries) => {
+			for (const { isIntersecting, target } of entries) {
 				if (!isIntersecting) continue;
-                const heading = getElementHeading(target);
-                // const heading = target
+				const heading = getElementHeading(target);
 				if (!heading) continue;
 				const link = links.find((link) => link.hash === '#' + encodeURIComponent(heading.id));
 				if (link) {
@@ -73,11 +70,12 @@ export class StarlightTOC extends HTMLElement {
 		// Also observe direct children of `.content` to include elements before
 		// the first heading.
 		const toObserve = document.querySelectorAll('main [id], main [id] ~ *, main .content > *');
+
 		let observer: IntersectionObserver | undefined;
 		const observe = () => {
-            if (observer) observer.disconnect();
-            observer = new IntersectionObserver(setCurrent, { rootMargin: this.getRootMargin() });
-            toObserve.forEach((h) => observer!.observe(h));
+			if (observer) observer.disconnect();
+			observer = new IntersectionObserver(setCurrent, { rootMargin: this.getRootMargin() });
+			toObserve.forEach((h) => observer!.observe(h));
 		};
 		observe();
 
@@ -91,14 +89,14 @@ export class StarlightTOC extends HTMLElement {
 		});
 	}
 
-    private getRootMargin(): `-${number}px 0% ${number}px` {
-		const navBarHeight = document.querySelector('blog-header')?.getBoundingClientRect().height || 0;
+	private getRootMargin(): `-${number}px 0% ${number}px` {
+		const navBarHeight = document.querySelector('header')?.getBoundingClientRect().height || 0;
 		// `<summary>` only exists in mobile ToC, so will fall back to 0 in large viewport component.
 		const mobileTocHeight = this.querySelector('summary')?.getBoundingClientRect().height || 0;
-		// 	/** 元素有自身的margin-top，在距离视口顶部header高度-marginTop距离时，认为进入视口范围内，激活目录元素 */
-		const top = navBarHeight + mobileTocHeight + 32;
+		/** 元素有自身的margin-top，在距离视口顶部header高度-marginTop距离时，认为进入视口范围内，激活目录元素 */
+		const top = navBarHeight + mobileTocHeight + 60;
 		/** End intersections 1.5rem later. */
-		const bottom = top ;
+		const bottom = top + 24;
 		const height = document.documentElement.clientHeight;
 		return `-${top}px 0% ${bottom - height}px`;
 	}
